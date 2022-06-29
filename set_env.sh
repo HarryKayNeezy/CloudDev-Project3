89dbb563126eaaffc68888b5ccb1aa9cb0d96175
Harry Kay Ablor <harrykayablor@Harrys-MacBook-Air.local>
2022-06-29 22:30:40 +0000

    build

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=projectuda
-export POSTGRES_PASSWORD=projectuda
-export POSTGRES_HOST=projectuda.ciokpolsgp7g.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=projectuda
-export AWS_BUCKET=arn:aws:s3:::udagramproject3-dev
+export POSTGRES_USERNAME=udacitydevp3
+export POSTGRES_PASSWORD=udacitydevp3
+export POSTGRES_HOST=udacitydevp3.croizp0bgh6q.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udacitydevp3
+export AWS_BUCKET=arn:aws:s3:::udacity-dev-p3
 export AWS_REGION=us-east-1
-export AWS_PROFILE=devops2
+export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
